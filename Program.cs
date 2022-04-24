df8328a5d9b6a6be852d426316aea80d8cb124d3
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2022-04-24 15:46:53 +0100

    Arreglado la lectura del appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+// See https://aka.ms/new-console-template for more information
+namespace Connect_Azure_Storage_With_Csharp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config= new ConfigurationBuilder()
+                                    .AddJsonFile("appsettings.json", true, true)
+                                    .Build();
+
+            string getConectionString = config["connectionstring"];
+            Console.WriteLine("Hello World!" + getConectionString);
+        }
+    }
+}
